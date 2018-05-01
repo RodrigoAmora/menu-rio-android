546fe86d242c4413b9067e9e503e11d8d2946e2b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-05-01 05:00:47 -0300

    Adjusts

--- app/src/main/java/br/com/lazerrio/util/NetworkUtil.java
+++ app/src/main/java/br/com/lazerrio/util/NetworkUtil.java
@@ -1,8 +1,21 @@
 package br.com.lazerrio.util;
 
-/**
- * Created by rodrigoamora on 01/05/18.
- */
+import android.content.Context;
+import android.net.ConnectivityManager;
 
 public class NetworkUtil {
+
+    public static boolean checkConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        boolean mobileIsConnected = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
+        boolean wifiIsConnected = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
+
+        if (!mobileIsConnected && !wifiIsConnected) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
