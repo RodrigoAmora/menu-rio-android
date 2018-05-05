9c90e27bbfac71c7f9dc387846a9d8552836e656
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-05-05 07:26:10 -0300

    Updating the version of Google Play Services

---
+++ app/src/main/java/br/com/lazerrio/ui/activity/DetailsActivity.java
@@ -0,0 +1,8 @@
+package br.com.lazerrio.ui.activity;
+
+/**
+ * Created by rodrigoamora on 05/05/18.
+ */
+
+public class DetailsActivity {
+}
---
+++ app/src/main/java/br/com/lazerrio/ui/fragment/DetailsFragment.java
@@ -0,0 +1,8 @@
+package br.com.lazerrio.ui.fragment;
+
+/**
+ * Created by rodrigoamora on 05/05/18.
+ */
+
+public class DetailsFragment {
+}
