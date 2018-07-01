7f1be628466ab1334c45782df99c5a7424ff8ef8
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-07-01 01:16:46 -0300

    Adjusts

--- app/src/main/java/br/com/lazerrio/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/lazerrio/ui/activity/MainActivity.java
@@ -65,9 +65,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_about) {
+        if (id == R.id.action_about) {
             FragmentUtil.changeFragment(R.id.conatiner, AboutFragment.class, getSupportFragmentManager(), false, null);
         }
 
