b9846e070abbfd222fbcef2e7a62311399e75d67
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-10-02 22:41:52 -0300

    Adjusting the call of the fragments

--- app/src/main/java/br/com/lazerrio/util/FragmentUtil.java
+++ app/src/main/java/br/com/lazerrio/util/FragmentUtil.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-
 public class FragmentUtil {
 
     public static void changeFragment(int id, Fragment fragment, FragmentManager fragmentManager, boolean backstack, Bundle bundle) {
