bce04414639b88868ba27d45fc0ea40e5450c7a6
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2021-06-21 13:16:42 -0300

    Creating method setTimeout

--- app/src/main/java/br/com/lazerrio/factory/RetrofitFactory.java
+++ app/src/main/java/br/com/lazerrio/factory/RetrofitFactory.java
@@ -1,5 +1,8 @@
 package br.com.lazerrio.factory;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,7 +11,17 @@ public class RetrofitFactory {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(setTimeout())
                 .build();
         return retrofit;
+    }
+
+    private static OkHttpClient setTimeout() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+        return okHttpClient;
     }
 }
